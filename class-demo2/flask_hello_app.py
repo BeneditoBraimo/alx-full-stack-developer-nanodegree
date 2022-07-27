0f459fbc6547236f5cdb08446517e365bab3ca7e
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-27 15:37:38 +0200

    __repr__ method added

--- class-demo2/flask_hello_app.py
+++ class-demo2/flask_hello_app.py
@@ -13,6 +13,11 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+
+    def __repr__(self) -> str:
+        return f"<Person ID: {self.id}, name {self.name}>"
+
+
 # create database models
 db.create_all()
 @app.route('/')

e68af83c7612d70eb13c95bf9dc9376ad061f6e3
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-27 13:29:37 +0200

    flask application that connects to a database

---
+++ class-demo2/flask-db-connect.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('__name__')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@localhost:5432/example'
+db = SQLAlchemy(app)
+
+@app.route('/')
+
+def index():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
+
+# enable debug mode on run
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=3000)

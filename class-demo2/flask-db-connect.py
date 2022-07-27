f7b3c0f93588e025776dba9b4810dae51e345063
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-27 14:46:15 +0200

    added lines to desable the track modifications, and added lines to read a database record

--- class-demo2/flask-db-connect.py
+++ class-demo2/flask-db-connect.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask('__name__')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:abc@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MOTIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # class to be mapped to the database
@@ -17,7 +18,8 @@ db.create_all()
 @app.route('/')
 
 def index():
-    return "Hello, world!"
+    person = Person.query.first()
+    return "Hello, " + person.name
 
 
 if __name__ == "__main__":

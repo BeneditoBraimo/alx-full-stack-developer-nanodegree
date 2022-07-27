092db6d19d05fc2bfc9ae8775f6eb5073bdef9bd
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-27 15:24:38 +0200

    line 7 changed a missspelled word

--- class-demo2/flask_hello_app.py
+++ class-demo2/flask_hello_app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask('__name__')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:abc@localhost:5432/example'
-app.config['SQLALCHEMY_TRACK_MOTIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # class to be mapped to the database

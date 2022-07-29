2c390978244c3251ba1099de12b41605a64c85c4
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-29 18:33:02 +0200

    quiz app used through python interative

---
+++ lesson-5-quiz/quiz1.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<User ID: {self.id}, user name: {self.name}>"
+
+db.create_all()

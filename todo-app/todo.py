5b286ce08591d368c76ef913d61cd17b640c61f5
BeneditoBraimo <beneditobraimo@gmail.com>
2022-07-29 19:03:22 +0200

    Todo app folder added

---
+++ todo-app/todo.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo ID: {self.id}, description {self.description}>"
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run("0.0.0.0", port=3000)

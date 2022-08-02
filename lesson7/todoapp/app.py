429c5eda46e4b8df392d3ca5f414d7cbdd7b3721
BeneditoBraimo <beneditobraimo@gmail.com>
2022-08-02 18:26:15 +0200

    fixed a bug on app.config object

--- lesson7/todoapp/app.py
+++ lesson7/todoapp/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:abc@localhost:5432/todoapp"
-app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 

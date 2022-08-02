cf3498936967b6bdeafc84ab34bdcb30d193ce7b
BeneditoBraimo <beneditobraimo@gmail.com>
2022-08-02 18:18:22 +0200

    database connectivity added to the todo app script

--- lesson7/todoapp/app.py
+++ lesson7/todoapp/app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:abc@localhost:5432/todoapp"
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class Todos(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __rep__(self):
+        return f"<ID: {self.id}, description {self.description}>"
+
+    
 @app.route("/")
 def index():
     return render_template("index.html", data=[

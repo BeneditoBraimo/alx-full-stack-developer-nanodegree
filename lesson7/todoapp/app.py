e6b88f4271d8fe70be1bd20de5165c9206f3973a
BeneditoBraimo <beneditobraimo@gmail.com>
2022-08-04 21:18:44 +0200

    using sessions in controllers

--- lesson7/todoapp/app.py
+++ lesson7/todoapp/app.py
@@ -1,39 +1,42 @@
-from crypt import methods
-from flask import Flask, redirect, render_template, request, url_for
+import sys
+from flask import Flask, jsonify, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:abc@localhost:5432/todoapp"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key = True)
-    description = db.Column(db.String(), nullable=False)
-
-    def __rep__(self):
-        return f"<ID: {self.id}, description {self.description}>"
+   __tablename__ = 'todos'
+   id = db.Column(db.Integer, primary_key=True)
+   description =  db.Column(db.String(), nullable=False)
 
 
 db.create_all()
 
-@app.route("/")
-def index():
-    return render_template("index.html", data = Todo.query.all()
-
-        )
-
 @app.route('/todos/create', methods=['POST'])
-def create_Todo():
-    description = request.form.get('description')
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+def create_todo():
+   error = False
+   body = {}
+   try:
+      description = request.get_json()['description']
+      todo = Todo(description=description)
+      db.session.add(todo)
+      db.session.commit()
+      body['description'] = todo.description
+   except:
+      error = True
+      db.session.rollback()
+      print(sys.exec_info())
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run("0.0.0.0", port=3000)
+   finally:
+      db.session.close()
+   if error:
+      abort(400)
+   else:
+      return jsonify(body)
+
+@app.route('/')
+def index():
+    return render_template('index.html',data=Todo.query.all())

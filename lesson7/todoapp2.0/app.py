87cccd84f6bcef0734a25c9db14a436469f61657
BeneditoBraimo <beneditobraimo@gmail.com>
2022-08-08 10:47:07 +0200

    Lesson 8 folder, with the 'delete' capability added to todo app

--- lesson7/todoapp2.0/app.py
+++ lesson7/todoapp2.0/app.py
@@ -1,9 +1,8 @@
-from crypt import methods
+from email.policy import default
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask import request
 import sys
-
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,12 +12,13 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
-
+#migrate = Migrate(app, db)
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():   
